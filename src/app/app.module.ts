e546b0a9820a0395eea2b4e3aabb1367de4d415d
Md Imtiyaz <md.imtiyaz@in2ittech.com>
2024-08-28 17:33:54 +0530

    code Optimize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { DashboardComponent } from './home-module/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-   
+    AppRoutingModule, SharedModule, 
+
   ],
   providers: [],
   bootstrap: [AppComponent]
