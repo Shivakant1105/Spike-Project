f3a93dfe5355186851a6cb8c877271f61005381b
Gautam Yadav <gautamyadav1992002@gmail.com>
2024-09-04 16:10:42 +0530

    added dynamic sidebar color changes.

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -10,6 +10,75 @@ import { CommonService } from 'src/service/common.service';
 export class SidebarComponent implements OnInit {
   toggle!: boolean;
   unSub = new Subject();
+  
+  listOfSidebar: { name: string; icon: string; route: string; className: string }[] = [
+    {
+      name: 'Chats',
+      icon: 'message-circle',
+      route: 'menu/chat',
+      className: 'orange',
+    },
+    {
+      name: 'Calender',
+      icon: 'calendar',
+      route: 'menu/calender',
+      className: 'green',
+    },
+    {
+      name: 'Email',
+      icon: 'Mail',
+      route: 'menu/mail',
+      className: 'red',
+    },
+    {
+      name: 'Contact',
+      icon: 'phone',
+      route: 'menu/contacts',
+      className: 'blue',
+    },
+    {
+      name: 'Courses',
+      icon: 'book',
+      route: 'menu/course',
+      className: 'grey',
+    },
+    {
+      name: 'Employee',
+      icon: 'users',
+      route: 'menu/employee',
+      className: 'orange',
+    },
+    {
+      name: 'Notes',
+      icon: 'book-open',
+      route: 'menu/book-open',
+      className: 'green',
+    },
+    {
+      name: 'Tickets',
+      icon: 'minus-square',
+      route: 'menu/tickets',
+      className: 'red',
+    },
+    {
+      name: 'Invoice',
+      icon: 'folder',
+      route: 'menu/invoice',
+      className: 'blue',
+    },
+    {
+      name: 'Todo',
+      icon: 'edit',
+      route: 'menu/todo',
+      className: 'grey',
+    },
+    {
+      name: 'Taskboard',
+      icon: 'trello',
+      route: 'menu/taskboard',
+      className: 'orange',
+    },
+  ];
 
   constructor(private commonService: CommonService) {
     this.commonService.sideBarTogglebtn.pipe(takeUntil(this.unSub)).subscribe({
