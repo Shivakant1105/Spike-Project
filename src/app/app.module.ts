686e4a8ab23d4ba5a3d6d2dd0a65bf70375d326b
RaunakShrivastwa <kumarraunak4201@gmail.com>
2024-09-05 11:36:12 +0530

    resolve deleted file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { FeatherModule } from 'angular-feather';
+import { allIcons } from 'angular-feather/icons';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule, SharedModule,
-
+    AppRoutingModule,
+    SharedModule,
+    FeatherModule.pick(allIcons),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
