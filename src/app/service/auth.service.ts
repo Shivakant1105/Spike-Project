a23f89ba3c2762a043796b9f840aee6be213c0f6
Gautam Yadav <gautamyadav1992002@gmail.com>
2024-09-16 12:41:43 +0530

    add env variable

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  baseUrl: string = "http://99.99.99.185:8089/spike";
+   baseUrl: string =environment.baseUrl;
 
   constructor(private http: HttpClient) {}
 /**
