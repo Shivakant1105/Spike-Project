05eea8549fb0b7b0950fcb623c0c6c8fd1c4647a
abhilasha <abhilashasng04@gmail.com>
2024-09-17 16:01:38 +0530

    integrate reset password api

--- src/app/home-module/account-setting/account-setting.component.ts
+++ src/app/home-module/account-setting/account-setting.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+// import { ActivatedRoute } from '@angular/router';
+import { CommonService } from 'src/app/service/common.service';
 
 @Component({
   selector: 'app-account-setting',
@@ -6,10 +10,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account-setting.component.scss']
 })
 export class AccountSettingComponent implements OnInit {
-
-  constructor() { }
+  resetPasswordForm!: FormGroup
+  token!: string
+  constructor(private fb: FormBuilder, private commonService: CommonService, private route: Router) { }
 
   ngOnInit(): void {
+    this.resetPasswordForm = this.fb.group({
+      oldPassword: ['', Validators.required],
+      newPassword: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    }, { validator: this.customValidator });
+
+  
   }
 
+/**
+ * @description Handles password reset by validating the form, checking for mismatching passwords, and making a reset request.
+ * @author Abhilasha Singh
+ * @returns {void}
+ */
+
+  save(): void{
+    Object.values(this.resetPasswordForm.controls).forEach((control) => {
+      control.markAsTouched();
+    });
+    if(this.resetPasswordForm.valid){
+      console.log(this.resetPasswordForm.valid);
+      
+    const { oldPassword, newPassword } = this.resetPasswordForm.value;    
+    this.commonService.resetPassword(oldPassword, newPassword).subscribe((res:any)=>{
+      console.log(res);
+      
+      alert("Password has been changed successfully");    
+      this.route.navigate(['/auth/login']);       
+    })          
+    }
+    else {
+      console.log("form invalid");
+      
+    }
+  }
+
+/**
+ * @description Custom validator that checks if the new password and confirm password fields match, and ensures the new password is not the same as the old password.
+ * This validator returns errors for the following conditions:
+ * - `sameAsOldPassword`: The new password cannot be the same as the old password.
+ * - `passwordsMismatch`: The new password and confirm password must match.
+ * @param {FormGroup} formGroup - The form group containing the password fields. 
+ * @returns {{ [key: string]: any } | null} - Returns an object with validation errors if the conditions are not met, otherwise returns null.
+ */
+
+  customValidator(formGroup: FormGroup) : { [key: string]: any } | null{
+    const newPassword = formGroup.get('newPassword')?.value;
+    const confirmPassword = formGroup.get('confirmPassword')?.value; 
+    const oldPassword = formGroup.get('oldPassword')?.value; 
+    const errors: { [key: string]: any } = {};
+    if (oldPassword === newPassword  &&  oldPassword) {      
+      errors['sameAsOldPassword'] = true;
+    }
+    
+    if (newPassword != confirmPassword && confirmPassword ) {           
+      errors['passwordsMismatch'] = true;
+    }   
+    return Object.keys(errors).length > 0 ? errors : null;
+  }
 }
--- src/app/service/common.service.ts
+++ src/app/service/common.service.ts
@@ -1,11 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CommonService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+  baseUrl = environment.baseUrl
 
   sideBarTogglebtn = new BehaviorSubject(false);
 
@@ -16,5 +19,18 @@ export class CommonService {
    */
   setSideBarToggleBtn(flag: boolean): void {
     this.sideBarTogglebtn.next(flag);
+  }
+
+  /**
+ * @description This method allows a user to reset their password.
+ * @author Abhilasha Singh
+ * @param {string} oldPassword - The user's current password that needs to be verified before setting the new password.
+ * @param {string} newPassword - The user's desired new password.
+ * @returns {Observable<any>} An observable that emits the server's response.
+ */
+
+  resetPassword( oldPassword: string, newPassword: string): Observable <any> {  
+    const body = { oldPassword, newPassword };
+    return this.http.put(`${this.baseUrl}/user/reset-password`,body)
   }
 }
