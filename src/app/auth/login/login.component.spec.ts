11fded89cd54c334cd62aff1fafa1b7dddb72947
sengar306 <viveksengar921927@gmail.com>
2024-09-19 18:04:34 +0530

    testcase for loggerService and login Component

--- src/app/auth/login/login.component.spec.ts
+++ src/app/auth/login/login.component.spec.ts
@@ -1,25 +1,46 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 import { LoginComponent } from './login.component';
+import { AuthService } from 'src/app/service/auth.service';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let authService: jasmine.SpyObj<AuthService>;
+  let router: jasmine.SpyObj<Router>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
-  });
+    const authServiceSpy = jasmine.createSpyObj('AuthService', ['login', 'setDataInLocalStorage']);
+    const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
 
-  beforeEach(() => {
+    await TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule],
+      declarations: [LoginComponent],
+      providers: [
+        FormBuilder,
+        { provide: AuthService, useValue: authServiceSpy },
+        { provide: Router, useValue: routerSpy },
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    authService = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
+    router = TestBed.inject(Router) as jasmine.SpyObj<Router>;
+
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should call AuthService.login and navigate on successful login', () => {
+    const mockToken = 'mock-token';
+    authService.login.and.returnValue(of({ data: { token: mockToken } }));
+    component.loginForm.setValue({ username: 'testuser', password: 'password123' });
+    component.onSubmit();
+
+    expect(authService.login).toHaveBeenCalledWith(component.loginForm.value);
+    expect(authService.setDataInLocalStorage).toHaveBeenCalledWith('tkn', JSON.stringify(mockToken));
+    expect(router.navigate).toHaveBeenCalledWith(['home/dashboard']);
   });
 });
