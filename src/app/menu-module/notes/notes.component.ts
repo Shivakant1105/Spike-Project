f0fb2393c3240699854b07127aa4efff507f4832
RaunakShrivastwa <kumarraunak4201@gmail.com>
2024-09-30 17:29:03 +0530

    notes page bug fix

--- src/app/menu-module/notes/notes.component.ts
+++ src/app/menu-module/notes/notes.component.ts
@@ -2,23 +2,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-notes',
   templateUrl: './notes.component.html',
-  styleUrls: ['./notes.component.scss']
+  styleUrls: ['./notes.component.scss'],
 })
 export class NotesComponent implements OnInit {
-  toggle: boolean=false
+  toggle: boolean = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  /**  
-    * @description This is a notesSidebar.  
-    * @author Shiva Kant Mishra
-    */
+  /**
+   * @description This is a notesSidebar.
+   * @author Shiva Kant Mishra
+   */
 
   notesSidebar(): void {
     this.toggle = !this.toggle;
-
   }
 }
