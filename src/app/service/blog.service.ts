ac50b874be5233b5f6967e27f8897aa5c78d5c30
Gautam Yadav <gautamyadav1992002@gmail.com>
2024-09-30 18:44:19 +0530

    add comments component with test cases and update sidebar routes

--- src/app/service/blog.service.ts
+++ src/app/service/blog.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -32,5 +32,66 @@ export class BlogService {
   postBlog(data: FormData): Observable<any> {
     let url = `${this.baseUrl}/blog/create`;
     return this.http.post(url, data);
+  }
+  /**
+   * @description Fetches all comments associated with a specific blog post.
+   * @author Gautam Yadav
+   * @param {string} id - The ID of the blog post for which to retrieve all comments.
+   * @returns {Observable<any>} return Observable
+   */
+  getAllCommentById(id: string) {
+    return this.http.get(`${this.baseUrl}/comments/get/all/${id}`).pipe(
+      map((res: any) => {
+        return res;
+      })
+    );
+  }
+  /**
+   * @description Fetches a specific comment by its ID for a blog post.
+   * @author Gautam Yadav
+   * @param {string} id - The ID of the comment to retrieve.
+   * @returns {Observable<any>} - return Observable
+   */
+  getCommentById(id: string) {
+    return this.http.get(`${this.baseUrl}/comments/get/comment/${id}`).pipe(
+      map((res: any) => {
+        return res;
+      })
+    );
+  }
+  /**
+   * @description Updates an existing comment for a specific blog post using the provided payload data.
+   * @author Gautam Yadav
+   * @param {string} id - The ID of the comment to be updated.
+   * @param {any} payload -  any type
+   * @returns {Observable<any>} - return Observable
+   */
+  updateCommentById(id: string, payload: any) {
+    return this.http.put(`${this.baseUrl}/comments/update/${id}`, payload);
+  }
+  /**
+   * @description Creates a new comment for a specific blog post using the provided payload data.
+   * @author Gautam Yadav
+   * @param {string} id - The ID of the blog post to which the comment should be added.
+   * @param {any} payload - The data for the new comment (e.g., comment content).
+   * @returns {Observable<any>} - return Observable
+   */
+  createCommentById(id: string, payload: any) {
+    return this.http.post(
+      `${this.baseUrl}/comments/add-comment/${id}`,
+      payload
+    );
+  }
+  /**
+   * @description Deletes a specific comment from a blog post by providing the blog ID and the comment ID.
+   * @author Gautam Yadav
+   * @param {string} blogId - The ID of the blog post from which the comment is to be deleted.
+   * @param {string} commentId - The ID of the comment to delete.
+   * @returns {Observable<any>} - return Observable
+   */
+  deleteCommentById(blogId: string, commentId: string) {
+    return this.http.delete(
+      `${this.baseUrl}/comments/delete/${blogId}/${commentId}`
+    );
   }
 }
